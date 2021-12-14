1179147855f208a67862c98594c935571a616bcf
bblachut1 <bogojedi1@gmail.com>
2021-12-14 20:49:48 +0100

    add methods to Animal that are needed for project

--- src/main/java/agh/ics/oop/Animal.java
+++ src/main/java/agh/ics/oop/Animal.java
@@ -1,60 +1,51 @@
 package agh.ics.oop;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Animal implements IMapElement{
-    private MapDirection orientation = MapDirection.NORTH;
+    private int orientation;
     private Vector2d position;
     private final IWorldMap map;
-    private final String genotype;
+    private final int[] genotype;
     private final ArrayList<IPositionChangeObserver> observersList = new ArrayList<>();
-    public Animal(IWorldMap map){
-        this(map, new Vector2d(2,2));
-    }
-    public Animal(IWorldMap map, Vector2d initialPosition){
+    private int currentEnergy;
+
+    public Animal(IWorldMap map, Vector2d initialPosition, int[] genotype, int startEnergy){
         position = initialPosition;
         this.map = map;
-        StringBuilder i = new StringBuilder();
-        for (int j = 0; j < 32; j++) {
-            i.append(ThreadLocalRandom.current().nextInt(0, 8));
-        }
-        genotype = i.toString();
+        Arrays.sort(genotype);
+        this.genotype = genotype;
+        orientation = ThreadLocalRandom.current().nextInt(0, 8);
+        currentEnergy = startEnergy;
     }
 
     public Vector2d getPosition() {
         return position;
     }
 
-    public MapDirection getOrientation() {
-        return orientation;
-    }
-
-    @Override
-    public String toString(){
-        return orientation.toString();
-    }
-
-    public void move(MoveDirection direction){
-        switch (direction){
-            case RIGHT -> orientation = orientation.next();
-            case LEFT -> orientation = orientation.previous();
-            case FORWARD -> {
-                Vector2d destination = position.add(orientation.toUnitVector());
-                if(map.canMoveTo(destination)){
-                    Vector2d oldPos = position;
+    public void move(){
+        int orientationChange = genotype[ThreadLocalRandom.current().nextInt(0, 32)];
+        switch (orientationChange){
+            case 0 -> {
+                Vector2d oldPos = position;
+                Vector2d destination = position.add(map.getMoveVector(orientation));
+                if (map.canMoveTo(destination)) {
                     position = destination;
                     positionChanged(oldPos, destination);
                 }
             }
-            case BACKWARD -> {
-                Vector2d destination = position.add(orientation.toUnitVector().opposite());
-                if(map.canMoveTo(destination)){
-                    Vector2d oldPos = position;
+            case 4 -> {
+                Vector2d oldPos = position;
+                Vector2d destination = position.add(map.getMoveVector((orientation+4)%8));
+                if (map.canMoveTo(destination)) {
                     position = destination;
                     positionChanged(oldPos, destination);
                 }
             }
+            default -> orientation = (orientation+orientationChange)%8;
         }
     }
     public void addObserver(IPositionChangeObserver observer){
@@ -65,11 +56,51 @@ public class Animal implements IMapElement{
     }
     private void positionChanged(Vector2d oldPosition, Vector2d newPosition){
         for (IPositionChangeObserver observer : observersList){
-            observer.positionChanged(oldPosition, newPosition);
+            observer.positionChanged(this, oldPosition, newPosition);
         }
     }
 
-    public String getGenotype() {
+    public int[] getGenotype() {
         return genotype;
+    }
+
+    public int getCurrentEnergy() {
+        return currentEnergy;
+    }
+
+    public void changeCurrentEnergy(int energyChange) {
+        currentEnergy += energyChange;
+    }
+
+    public void makeChild(Animal other){
+        boolean strongerTakesLeft = new Random().nextBoolean();
+        Animal a1 = this;
+        Animal a2 = other;
+        int childEnergy = getCurrentEnergy()/4+ other.getCurrentEnergy()/4;
+        changeCurrentEnergy(-getCurrentEnergy()/4);
+        other.changeCurrentEnergy(-other.getCurrentEnergy()/4);
+        int[] childGenotype = new int[32];
+        if (currentEnergy < other.getCurrentEnergy()) {
+            a1 = other;
+            a2 = this;
+        }
+        int[] a1Gen = a1.getGenotype();
+        int[] a2Gen = a2.getGenotype();
+        for (int i = 0; i < 32; i++) {
+            if (strongerTakesLeft){
+                if (i<a1.getCurrentEnergy()*32/a2.getCurrentEnergy()) {
+                    childGenotype[i] = a1Gen[i];
+                }else {
+                    childGenotype[i] = a2Gen[i];
+                }
+            }else {
+                if (i<32-(a1.getCurrentEnergy()*32/a2.getCurrentEnergy())) {
+                    childGenotype[i] = a2Gen[i];
+                }else {
+                    childGenotype[i] = a1Gen[i];
+                }
+            }
+        }
+       map.place(new Animal(map, position, childGenotype, childEnergy));
     }
 }
